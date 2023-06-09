970e74768e8c8a6e9bd07f59aae4ef8e417a6e1f
wangjian <2307742375@qq.com>
2023-06-09 21:13:14 +0800

    1

--- src-capacitor/android/app/src/main/java/org/capacitor/quasar/app/MainActivity.java
+++ src-capacitor/android/app/src/main/java/org/capacitor/quasar/app/MainActivity.java
@@ -1,5 +1,19 @@
 package org.capacitor.quasar.app;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // 启用 WebView 的跨域请求
+    WebView webView = this.bridge.getWebView();
+    WebSettings webSettings = webView.getSettings();
+    webSettings.setAllowUniversalAccessFromFileURLs(true);
+  }
+}
